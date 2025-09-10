7a3bc42a7ead27f37589f1729b53ed48d4a315e6
Sahil07021997 <sahilkumardhiman07@gmail.com>
2025-09-10 08:11:22 +0530

    comments

--- fastapi/TodoApp/models.py
+++ fastapi/TodoApp/models.py
@@ -3,11 +3,12 @@ from sqlalchemy import Column, Integer, String, Boolean
 from database import Base
 
 
+# Define Todos table (ORM model)
 class Todos(Base):
-    __tablename__ = "todos"
+    __tablename__ = "todos"  # table name in DB
 
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    priority = Column(Integer, index=True)
-    completed = Column(Boolean, default=False)
+    id = Column(Integer, primary_key=True, index=True)  # unique ID
+    title = Column(String, index=True)  # short title
+    description = Column(String, index=True)  # details
+    priority = Column(Integer, index=True)  # priority number
+    completed = Column(Boolean, default=False)  # task status
