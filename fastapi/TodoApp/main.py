5f101d8fb86d6e77405a4f8440c878061fbcec76
Sahil07021997 <sahilkumardhiman07@gmail.com>
2025-09-10 08:10:50 +0530

    comments

--- fastapi/TodoApp/main.py
+++ fastapi/TodoApp/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-
 import models
 from database import engine
 
+# Create FastAPI instance
 app = FastAPI()
 
+# Create database tables from models (if not already created)
 models.Base.metadata.create_all(bind=engine)
-
