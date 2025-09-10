52dfb56e9470cbd40d75f063256b451e87538e39
Sahil07021997 <sahilkumardhiman07@gmail.com>
2025-09-10 08:10:16 +0530

    adding comments

--- fastapi/TodoApp/database.py
+++ fastapi/TodoApp/database.py
@@ -1,10 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
+# Database URL (SQLite in this case)
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
+# Engine: main connection to the database
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
+# Session factory: creates DB sessions
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Base class for ORM models
 Base = declarative_base()
