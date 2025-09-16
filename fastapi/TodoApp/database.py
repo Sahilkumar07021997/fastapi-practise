bd0d5e0024ae9de13b927889a5fe881bc7fa808b
Sahil07021997 <sahilkumardhiman07@gmail.com>
2025-09-16 08:24:23 +0530

    adding Database configuration file

--- fastapi/TodoApp/database.py
+++ fastapi/TodoApp/database.py
@@ -2,13 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Database URL (SQLite in this case)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
 # Engine: main connection to the database
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 # Session factory: creates DB sessions
-session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Base class for ORM models
 Base = declarative_base()
