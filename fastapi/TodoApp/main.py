027b12670b85a96415d8a620f50006bba86bd04e
Sahil07021997 <sahilkumardhiman07@gmail.com>
2025-10-06 08:46:43 +0530

    Adding authorization dependency

--- fastapi/TodoApp/main.py
+++ fastapi/TodoApp/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 import models
 from database import engine
-from routers import auth, todos
+from routers import auth, todos, admin
 
 # Create FastAPI instance
 app = FastAPI()
@@ -13,3 +13,4 @@ models.Base.metadata.create_all(bind=engine)
 # Include routers (if any)
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(admin.router)
